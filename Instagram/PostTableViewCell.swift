83cd8e996ef76fdd5dd2d354dd51c6e9213e1563
ponte1010 <t.miwa.528@nitech.jp>
2020-12-22 22:24:41 +0900

    [add]投稿データを表示するカスタムTableViewCell

---
+++ Instagram/PostTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  PostTableViewCell.swift
+//  Instagram
+//
+//  Created by TECH_ACADEMY on 2020/12/22.
+//  Copyright © 2020 tetsushi.miwa. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var likeLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
