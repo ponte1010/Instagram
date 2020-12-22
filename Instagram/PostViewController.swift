83a2e4d890ea1002a147c5ac9a89aef60cc16157
ponte1010 <t.miwa.528@nitech.jp>
2020-12-22 21:05:37 +0900

    [add]投稿画面の表示

--- Instagram/PostViewController.swift
+++ Instagram/PostViewController.swift
@@ -7,7 +7,27 @@
 //
 
 import UIKit
+import Firebase
+import SVProgressHUD
 
 class PostViewController: UIViewController{
+    var image: UIImage!
     
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textField: UITextField!
+    
+    // 投稿ボタンをタップしたときに呼ばれるメソッド
+    @IBAction func handlePostButton(_ sender: Any) {
+    }
+    
+    // キャンセルボタンをタップしたときに呼ばれるメソッド
+    @IBAction func handleCancelButton(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 受け取った画像をImageViewに設定する
+        imageView.image = image
+    }
 }
