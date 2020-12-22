cd08b3ed318d2fd3c869e804deaac496beaf7c42
ponte1010 <t.miwa.528@nitech.jp>
2020-12-22 21:16:54 +0900

    [add]投稿処理

---
+++ Instagram/Const.swift
@@ -0,0 +1,14 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by TECH_ACADEMY on 2020/12/22.
+//  Copyright © 2020 tetsushi.miwa. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
