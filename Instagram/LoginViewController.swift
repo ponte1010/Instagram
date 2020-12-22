e3f003292204452b529331da5a77e1a17eda2f96
ponte1010 <t.miwa.528@nitech.jp>
2020-12-22 11:08:27 +0900

    [add]ログイン処理機能

--- Instagram/LoginViewController.swift
+++ Instagram/LoginViewController.swift
@@ -17,6 +17,24 @@ class LoginViewController: UIViewController{
     
     // ログインボタンをタップしたときに呼ばれるメソッド
     @IBAction func handleLoginButton(_ sender: Any) {
+        if let address = mailAddressTextField.text, let password = passwordTextField.text {
+            
+            // アドレスとパスワード名のいずれかでも入力されていない時は何もしない
+            if address.isEmpty || password.isEmpty {
+                return
+            }
+            
+            Auth.auth().signIn(withEmail: address, password: password) { authResult, error in
+                if let error = error {
+                    print("DEBUG_PRINT: " + error.localizedDescription)
+                    return
+                }
+                print("DEBUG_PRINT: ログインに成功しました。")
+                
+                // 画面を閉じてタブ画面に戻る
+                self.dismiss(animated: true, completion: nil)
+            }
+        }
     }
     
     // アカウント作成ボタンをタップしたときに呼ばれるメソッド
