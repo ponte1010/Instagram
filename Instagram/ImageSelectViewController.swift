f9c26b16ca80ed92e93fe58f038a82e9b2d79cc8
ponte1010 <t.miwa.528@nitech.jp>
2020-12-22 20:40:10 +0900

    [add]画像加工画面の表示

--- Instagram/ImageSelectViewController.swift
+++ Instagram/ImageSelectViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import CLImageEditor
 
-class ImageSelectViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+class ImageSelectViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, CLImageEditorDelegate{
     @IBAction func handleLibraryButton(_ sender: Any) {
         // ライブラリ（カメラロール）を指定してピッカーを開く
         if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
@@ -34,25 +35,44 @@ class ImageSelectViewController: UIViewController, UIImagePickerControllerDelega
         self.dismiss(animated: true, completion: nil)
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
     // 写真を撮影/選択したときに呼ばれるメソッド
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if info[.originalImage] != nil {
             // 撮影/選択された画像を取得する
             let image = info[.originalImage] as! UIImage
-
+            
             // あとでCLImageEditorライブラリで加工する
             print("DEBUG_PRINT: image = \(image)")
-
+            // CLImageEditorにimageを渡して、加工画面を起動する。
+            let editor = CLImageEditor(image: image)!
+            editor.delegate = self
+            editor.modalPresentationStyle = .fullScreen
+            picker.present(editor, animated: true, completion: nil)
         }
     }
-
+    
+    // CLImageEditorで加工が終わったときに呼ばれるメソッド
+    func imageEditor(_ editor: CLImageEditor!, didFinishEditingWith image: UIImage!) {
+        // 投稿画面を開く
+        let postViewController = self.storyboard?.instantiateViewController(withIdentifier: "Post") as! PostViewController
+        postViewController.image = image!
+        editor.present(postViewController, animated: true, completion: nil)
+    }
+    
+    // CLImageEditorの編集がキャンセルされた時に呼ばれるメソッド
+    func imageEditorDidCancel(_ editor: CLImageEditor!) {
+        // ImageSelectViewController画面を閉じてタブ画面に戻る
+        self.presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+    
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         // ImageSelectViewController画面を閉じてタブ画面に戻る
         self.presentingViewController?.dismiss(animated: true, completion: nil)
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
     
 }
